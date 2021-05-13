dc856d4008c2911603543dd7954591f3bc075c90
kevinoo <lucichkevin@gmail.com>
2021-05-13 23:58:35 +0200

    Fixed Parser.php

---
+++ src/Parser.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace kevinoo\QueryParser;
+
+use Exception;
+use kevinoo\QueryParser\Statement\{
+    AbstractStatement,
+    InsertStatement,
+    UpdateStatement,
+    DeleteStatement,
+    SelectStatement
+};
+
+
+/**
+ *
+ */
+class Parser {
+
+    const MYSQL_DELIMITER = '/*!*/;';
+    const MYSQL_DUMP_DELIMITER = self::MYSQL_DELIMITER ."\n". self::MYSQL_DELIMITER;
+
+
+    /**
+     * @return InsertStatement|UpdateStatement|DeleteStatement|SelectStatement
+     * @throws Exception
+     */
+    public static function parse( string $statement_string ){
+
+        $statement_string = str_replace( [static::MYSQL_DELIMITER,"\t","\n","\r"],['',' ',' ',' '],$statement_string);
+
+        print_r($statement_string);
+        echo "\n\n";
+
+        if( stripos($statement_string,'INSERT INTO ') !== false ){
+            $statement = new InsertStatement();
+        }elseif( stripos($statement_string,'UPDATE ') !== false ){
+            $statement = new UpdateStatement();
+        }elseif( stripos($statement_string,'DELETE FROM ') !== false ){
+            $statement = new DeleteStatement();
+        }elseif( stripos($statement_string,'SELECT ') !== false ){
+            $statement = new SelectStatement();
+        }else{
+            throw new Exception('Query not managed');
+        }
+
+        $statement->setQuery($statement_string);
+        $statement->parseQuery();
+
+        return $statement;
+    }
+
+}
