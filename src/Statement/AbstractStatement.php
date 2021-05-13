9d7108f9600369ea37c956ca718700711c696b2b
kevinoo <lucichkevin@gmail.com>
2021-05-13 23:30:57 +0200

    Added query, columns, values and where_conditions attribute to AbstractStatement

--- src/Statement/AbstractStatement.php
+++ src/Statement/AbstractStatement.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace kevinoo\QueryParser;
+namespace kevinoo\QueryParser\Statement;
 
-use DateTime;
+use Exception;
 
 
 /**
@@ -10,13 +10,24 @@ use DateTime;
 */
 abstract class AbstractStatement {
 
+    protected string $query;
     protected string $table_name;
+    protected array $columns = [];
+    protected array $values = [];
+    protected array $where_conditions = [];
 
+    /**
+     * @throws Exception
+     */
+    abstract public function parseQuery();
 
 
     ///////////////////////////////////////
     // Utils
 
+    public function isSelectQuery(): bool {
+        return false;
+    }
     public function isInsertQuery(): bool {
         return false;
     }
@@ -25,6 +36,24 @@ abstract class AbstractStatement {
     }
     public function isDeleteQuery(): bool {
         return false;
+    }
+
+    /**
+     *
+     * @param $columns_string
+     * @return array
+     */
+    protected function normalizeColumnsString( $columns_string ): array {
+        return [];
+    }
+
+    /**
+     *
+     * @param $values_string
+     * @return array
+     */
+    protected function normalizeValuesString( $values_string ): array {
+        return [];
     }
 
     ///////////////////////////////////////
@@ -36,13 +65,73 @@ abstract class AbstractStatement {
     public function getTableName(): string {
         return $this->table_name;
     }
-
     /**
      * @param string $table_name
-     * @return Statement
+     * @return self
      */
     public function setTableName( string $table_name ): self {
         $this->table_name = $table_name;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getQuery(): string {
+        return $this->query;
+    }
+    /**
+     * @param string $query
+     * @return self
+     */
+    public function setQuery( string $query ): self {
+        $this->query = $query;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getColumns(): array {
+        return $this->columns;
+    }
+
+    /**
+     * @param array $columns
+     * @return self
+     */
+    public function setColumns( array $columns ): self {
+        $this->columns = $columns;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getValues(): array {
+        return $this->values;
+    }
+    /**
+     * @param array $values
+     * @return self
+     */
+    public function setValues( array $values ): self {
+        $this->values = $values;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function getWhereConditions(): array {
+        return $this->where_conditions;
+    }
+    /**
+     * @param array $where_conditions
+     * @return self
+     */
+    public function setWhereConditions( array $where_conditions ): self {
+        $this->where_conditions = $where_conditions;
         return $this;
     }
 
